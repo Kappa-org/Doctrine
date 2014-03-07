e717239d3e67b776892ecd446895cfabc6fab882
Budry <zarubaondra@gmail.com>
2014-03-07 12:57:30 +0100

    Changed structure

--- src/Kappa/Doctrine/Entity/Entity.php
+++ src/Kappa/Doctrine/Entity/Entity.php
@@ -8,7 +8,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Kappa\Doctrine;
+namespace Kappa\Doctrine\Entity;
 
 use Kdyby\Doctrine\Entities\BaseEntity;
 use Doctrine\ORM\Mapping as ORM;
