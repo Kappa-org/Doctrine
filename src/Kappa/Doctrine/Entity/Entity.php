0e9dbbb72081d43f4a17056749a8f9d0f6668217
Budry <zarubaondra@gmail.com>
2014-04-27 16:11:49 +0200

    Method fill is replaced by EntityHydrator

--- src/Kappa/Doctrine/Entity/Entity.php
+++ src/Kappa/Doctrine/Entity/Entity.php
@@ -29,35 +29,10 @@ abstract class Entity extends BaseEntity
 	protected $id;
 
 	/**
-	 * @param array $data
-	 */
-	public function __construct(array $data = array())
-	{
-		$this->fill($data);
-	}
-
-	/**
 	 * @return int
 	 */
 	public function getId()
 	{
 		return $this->id;
-	}
-
-	/**
-	 * @param array $data
-	 * @throws \Kappa\Doctrine\InvalidPropertyNameException
-	 */
-	public function fill(array $data = array())
-	{
-		$properties = get_object_vars($this);
-		foreach ($data as $key => $value) {
-			if (!array_key_exists($key, $properties)) {
-				throw new InvalidPropertyNameException("Unknown property '{$key}'");
-			}
-			$setterName = 'set';
-			$setterName .= ucfirst($key);
-			$this->$setterName($value);
-		}
 	}
 }
