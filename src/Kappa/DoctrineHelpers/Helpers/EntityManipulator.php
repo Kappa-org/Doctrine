f269f69dc06cc2dcc059179ddcab8734a5f878ab
Budry <zarubaondra@gmail.com>
2014-12-27 00:19:14 +0100

    Added entity manipulator

---
+++ src/Kappa/DoctrineHelpers/Helpers/EntityManipulator.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * This file is part of the Kappa\DoctrineHelpers package.
+ *
+ * (c) Ondřej Záruba <zarubaondra@gmail.com>
+ *
+ * For the full copyright and license information, please view the license.md
+ * file that was distributed with this source code.
+ */
+
+namespace Kappa\DoctrineHelpers\Helpers;
+
+use Doctrine\Common\Collections\Collection;
+use Nette\Object;
+use Nette\Utils\Callback;
+
+/**
+ * Class EntityManipulator
+ *
+ * @package Kappa\DoctrineHelpers\Helpers
+ * @author Ondřej Záruba <http://zaruba-ondrej.cz>
+ */
+class EntityManipulator extends Object
+{
+	const SET_TYPE = "set";
+
+	const ADD_TYPE = "add";
+
+	const GET_TYPE = "get";
+
+	/**
+	 * @param object $entity
+	 * @param string $column
+	 * @param mixed $value
+	 * @param string $type
+	 */
+	public function invoke($entity, $column, $value, $type)
+	{
+		$ref = new \ReflectionProperty($entity, $column);
+		if ($ref->isPublic()) {
+			if ($ref->getValue($entity) instanceof Collection) {
+				$ref->getValue($entity)->add($value);
+			} else {
+				$ref->setValue($entity, $value);
+			}
+		} else {
+			Callback::invokeArgs([$entity, $this->getMethodName($type, $column)], [$value]);
+		}
+	}
+
+	/**
+	 * @param string $type
+	 * @param string $column
+	 * @return string
+	 */
+	private function getMethodName($type, $column)
+	{
+		$name = $type;
+		if ($type == self::ADD_TYPE) {
+			if (substr($column, -3) == 'ies') {
+				$column = substr($column, 0, strlen($column) - 3) . 'y';
+			} elseif (substr($column, -1) == 's') {
+				$column = substr($column, 0, strlen($column) - 1);
+			}
+		}
+		$name .= ucfirst($column);
+
+		return $name;
+	}
+}
