c768161ec813016262f296094ac9be596cf291d9
Budry <zarubaondra@gmail.com>
2014-12-27 19:32:30 +0100

    Added entity for relations mapper test

--- tests/KappaTests/bootstrap.php
+++ tests/KappaTests/bootstrap.php
@@ -15,6 +15,7 @@ if ((!$loader = @include __DIR__ . '/../../vendor/autoload.php') && (!$loader = 
 $loader->add('KappaTests', __DIR__ . '/../');
 
 require __DIR__ . '/Entities/GlobalEntity.php';
+require __DIR__ . '/Entities/RelationIdEntity.php';
 
 // configure environment
 Tester\Environment::setup();
