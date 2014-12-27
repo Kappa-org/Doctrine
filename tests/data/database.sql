7c8413c8b0c050495b8cccdec8408bd9a8a73c94
Budry <zarubaondra@gmail.com>
2014-12-27 19:37:44 +0100

    Refactored databse SQL for tests

---
+++ tests/data/database.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE IF NOT EXISTS test;
+
+CREATE TABLE `relations_id` (
+  `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `parent_id` int NULL
+) ENGINE='InnoDB' COLLATE 'utf8_czech_ci';
+
+ALTER TABLE `relations_id`
+ADD INDEX `parent_id` (`parent_id`);
+
+ALTER TABLE `relations_id`
+ADD FOREIGN KEY (`parent_id`) REFERENCES `relations_id` (`id`)
+
+INSERT INTO `relations_id` (`parent_id`)
+VALUES (NULL);
