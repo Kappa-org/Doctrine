a0c1cb85deb96b569b60ac5c9ed371e50790f472
Budry <zarubaondra@gmail.com>
2015-01-27 22:01:06 +0100

    Added base test case for orm

---
+++ tests/KappaTests/ORMTestCase.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This file is part of the doctrine package.
+ *
+ * (c) Ondřej Záruba <zarubaondra@gmail.com>
+ *
+ * For the full copyright and license information, please view the license.md
+ * file that was distributed with this source code.
+ */
+
+namespace KappaTests;
+
+use Tester\TestCase;
+
+/**
+ * Class ORMTestCase
+ *
+ * @package KappaTests
+ * @author Ondřej Záruba <http://zaruba-ondrej.cz>
+ */
+class ORMTestCase extends TestCase
+{
+	protected $em;
+
+	protected function setUp()
+	{
+		$container = getContainer();
+		$this->em = $container->getByType('Kdyby\Doctrine\EntityManager');
+	}
+}
