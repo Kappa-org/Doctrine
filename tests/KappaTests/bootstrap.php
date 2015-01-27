953694f7743b809ff7d1cf2ad0a3f9dea995e668
Budry <zarubaondra@gmail.com>
2015-01-27 16:15:41 +0100

    Removed useless configuration

--- tests/KappaTests/bootstrap.php
+++ tests/KappaTests/bootstrap.php
@@ -15,22 +15,11 @@ if ((!$loader = @include __DIR__ . '/../../vendor/autoload.php') && (!$loader = 
 /** @var Composer\Autoload\ClassLoader $loader */
 $loader->addPsr4("KappaTests\\", __DIR__);
 
-
 // configure environment
 Tester\Environment::setup();
-class_alias('Tester\Assert', 'Assert');
-date_default_timezone_set('Europe/Prague');
-// create temporary directory
+
 define('TEMP_DIR', __DIR__ . '/../temp/' . (isset($_SERVER['argv']) ? md5(serialize($_SERVER['argv'])) : getmypid()));
 Tester\Helpers::purge(TEMP_DIR);
-$_SERVER = array_intersect_key($_SERVER, array_flip(array(
-	'PHP_SELF', 'SCRIPT_NAME', 'SERVER_ADDR', 'SERVER_SOFTWARE', 'HTTP_HOST', 'DOCUMENT_ROOT', 'OS', 'argc', 'argv')));
-$_SERVER['REQUEST_TIME'] = 1234567890;
-$_ENV = $_GET = $_POST = array();
-
-function id($val) {
-	return $val;
-}
 
 function getContainer()
 {
